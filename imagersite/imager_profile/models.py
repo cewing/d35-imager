289d54130511788606e2c402d818e414642a6708
cewing <cris@crisewing.com>
2015-07-20 21:16:32 -0700

    first pass at profile model

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+PHOTOGRAPHY_TYPE_CHOICES = [
+    ('Portrait', 'Portrait Photography'),
+    ('Landscape', 'Landscape Photography'),
+    ('Still-life', 'Still-life Photography'),
+]
+
+
+class ActiveProfileManager(models.Manager):
+    """A custom model manager limited only to active profiles"""
+    def get_queryset(self):
+        """Filter the default queryset for active users"""
+        qs = super(ActiveProfileManager, self).get_queryset()
+        return qs.filter(user__is_active=True)
+
+
+@python_2_unicode_compatible
+class ImagerProfile(models.Model):
+    user = models.OneToOneField(User)
+    camera = models.CharField(
+        max_length=128,
+        help_text="What is the make and model of your camera?"
+    )
+    address = models.TextField()
+    website_url = models.URLField()
+    photography_type = models.CharField(
+        max_length=64,
+        help_text="What type of photography do you primarily make?",
+        choices=PHOTOGRAPHY_TYPE_CHOICES
+    )
+
+    objects = models.Manager()
+    active = ActiveProfileManager()
+
+    def is_active(self):
+        return self.user.is_active
+
