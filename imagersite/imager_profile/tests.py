dfa4f37ea07a99fcbc2245f1b0f7ccdc43aaf5ea
cewing <cris@crisewing.com>
2015-07-23 10:39:09 -0700

    make a useful test or two

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,31 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
 from django.test import TestCase
+import factory
 
-# Create your tests here.
+from imager_profile.models import ImagerProfile
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: "user{}".format(n))
+    email = factory.Sequence(lambda n: "user{}@example.com".format(n))
+
+
+class ProfileTestCase(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory.build()
+
+    def test_profile_is_created_when_user_is_saved(self):
+        self.assertTrue(ImagerProfile.objects.count() == 0)
+        self.user.save()
+        self.assertTrue(ImagerProfile.objects.count() == 1)
+
+    def test_profile_str_is_user_username(self):
+        self.user.save()
+        profile = ImagerProfile.objects.get(user=self.user)
+        self.assertEqual(str(profile), self.user.username)
+
